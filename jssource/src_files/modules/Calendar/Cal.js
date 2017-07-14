897c202ac97954d0e1a8cb2f235796137c42d426
ebogaard <erwin.bogaard@gmail.com>
2017-07-14 10:38:30 +0200

    Auto-close Calendar pop-ups after mouse leave
    Fixes Calendar issue in #3756

--- jssource/src_files/modules/Calendar/Cal.js
+++ jssource/src_files/modules/Calendar/Cal.js
@@ -1082,7 +1082,11 @@ $($.fullCalendar).ready(function () {
 							at: 'top left'
 						},
 						show: {solo: true},
-						hide: {event: false},
+						hide: {
+							event: 'mouseleave',
+							fixed: true,
+							delay: 500
+						},
 						style: {
 							width: 224,
 							padding: 5,
